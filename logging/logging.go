81f109ed2ffe5fb455d63eefb7e86a431f195e66
Diogo Correia <diogorodriguescorreia89@gmail.com>
2023-09-13 00:08:46 +0100

    Initialize project

---
+++ logging/logging.go
@@ -0,0 +1,46 @@
+package logging
+
+import (
+	"log"
+	"os"
+
+	"github.com/google/uuid"
+)
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "", 0)
+	WarningLogger = log.New(file, "", 0)
+	ErrorLogger = log.New(file, "", 0)
+
+	requestUuid := uuid.New().String()
+
+	InfoLogger.SetPrefix("[INFO]: " + requestUuid + " ")
+	WarningLogger.SetPrefix("[WARNING]: " + requestUuid + " ")
+	ErrorLogger.SetPrefix("[ERROR]: " + requestUuid + " ")
+}
+
+func LogInfo(message string) {
+	InfoLogger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	InfoLogger.Println(message)
+}
+
+func LogWarning(message string) {
+	InfoLogger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	WarningLogger.Println(message)
+}
+
+func LogError(message string) {
+	InfoLogger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	ErrorLogger.Println(message)
+}

8dd2a7bf860ec884ddb905dc60a5d01aa05c0487
Diogo Correia <diogorodriguescorreia89@gmail.com>
2024-08-15 00:02:28 +0100

    Added: docker container with mysql, connect application with mysql.

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"sort"
+    "fmt"
+    "log"
+    "net/http"
+    "database/sql"
 
-	"diogocorreia.com/golang-application/logging"
+    _ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	logging.LogInfo("Init Execution of Application")
-
-	names := []string{"Portugal", "England", "Brazil", "Argentina", "Uruguay", "Germany", "Italy", "Indonesia", "India", "France", "Spain"}
-
-	sort.Strings(names)
-	for _, element := range names {
-		fmt.Println(transform(element))
-	}
-
-	logging.LogInfo("End Execution of Application")
+type Product struct {
+    ProductId int
+    ProductName string
 }
 
-func transform(name string) string {
-	return "Hello " + name
+func main() {
+    dsn := "user:password@tcp(127.0.0.1:3306)/lutetium"
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Query to select data
+    rows, err := db.Query("SELECT product_id, product_name FROM products")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    // Iterate over the rows
+    for rows.Next() {
+        var productId int
+        var productName string
+
+        if err := rows.Scan(&productId, &productName); err != nil {
+            log.Fatal(err)
+        }
+
+        product := Product{ProductId: productId, ProductName: productName}
+
+        fmt.Printf("Id: %d Name: %s\n", product.ProductId, product.ProductName)
+    }
+
+    // Check for errors after iterating over rows
+    if err := rows.Err(); err != nil {
+        log.Fatal(err)
+    }
+    
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
